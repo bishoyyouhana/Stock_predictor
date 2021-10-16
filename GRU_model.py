1572c779b671cb3c5fad9d7bae86b0f2f3c53e6f
Bishoy <youhanna58@gmail.com>
2021-10-16 00:39:34 -0400

    initial commit

---
+++ GRU_model.py
@@ -0,0 +1,217 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn as nn
+import time
+import math
+from sklearn.metrics import mean_squared_error
+'''
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import chart_studio.plotly as py
+import chart_studio
+'''
+
+input_dim = 1
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+num_epochs = 40
+
+class GRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(GRU, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn) = self.gru(x, (h0.detach()))
+        out = self.fc(out[:, -1, :]) 
+        return out
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :]) 
+        return out
+
+data = pd.read_csv('IBM_2006-01-01_to_2018-01-01.csv')
+data = data.sort_values('Date')
+price = data[['Close']]
+scaler = MinMaxScaler(feature_range=(-1, 1))
+price = scaler.fit_transform(price.values.reshape(-1,1))
+
+def split_data(stock, lookback):
+    data_raw = stock #.to_numpy() # convert to numpy array
+    data = []
+    
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - lookback): 
+        data.append(data_raw[index: index + lookback])
+    
+    data = np.array(data);
+    test_set_size = int(np.round(0.2*data.shape[0]));
+    train_set_size = data.shape[0] - (test_set_size);
+    
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
+    
+    x_test = data[train_set_size:,:-1]
+    y_test = data[train_set_size:,-1,:]
+    
+    return [x_train, y_train, x_test, y_test]
+
+lookback = 20 # choose sequence length
+x_train, y_train, x_test, y_test = split_data(price, lookback)
+print('x_train.shape = ',x_train.shape)
+print('y_train.shape = ',y_train.shape)
+print('x_test.shape = ',x_test.shape)
+print('y_test.shape = ',y_test.shape)
+
+x_train = torch.from_numpy(x_train).type(torch.Tensor)
+x_test = torch.from_numpy(x_test).type(torch.Tensor)
+#y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor)
+#y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor)
+y_train_gru = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_gru = torch.from_numpy(y_test).type(torch.Tensor)
+
+
+model = GRU(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+criterion = torch.nn.MSELoss(reduction='mean')
+optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+
+hist = np.zeros(num_epochs)
+start_time = time.time()
+gru = []
+
+for t in range(num_epochs):
+    y_train_pred = model(x_train)
+
+    loss = criterion(y_train_pred, y_train_gru)
+    print("Epoch ", t, "MSE: ", loss.item())
+    hist[t] = loss.item()
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+training_time = time.time()-start_time    
+print("Training time: {}".format(training_time))
+
+# make predictions
+y_test_pred = model(x_test)
+
+# invert predictions
+y_train_pred = scaler.inverse_transform(y_train_pred.detach().numpy())
+y_train = scaler.inverse_transform(y_train_gru.detach().numpy())
+y_test_pred = scaler.inverse_transform(y_test_pred.detach().numpy())
+y_test = scaler.inverse_transform(y_test_gru.detach().numpy())
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(y_train[:,0], y_train_pred[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(y_test[:,0], y_test_pred[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+gru.append(trainScore)
+gru.append(testScore)
+gru.append(training_time)
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(price)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[lookback:len(y_train_pred)+lookback, :] = y_train_pred
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(price)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(y_train_pred)+lookback-1:len(price)-1, :] = y_test_pred
+
+original = scaler.inverse_transform(price.reshape(-1,1))
+
+predictions = np.append(trainPredictPlot, testPredictPlot, axis=1)
+predictions = np.append(predictions, original, axis=1)
+result = pd.DataFrame(predictions)
+
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(go.Scatter(x=result.index, y=result[0],
+                    mode='lines',
+                    name='Train prediction')))
+fig.add_trace(go.Scatter(x=result.index, y=result[1],
+                    mode='lines',
+                    name='Test prediction'))
+fig.add_trace(go.Scatter(go.Scatter(x=result.index, y=result[2],
+                    mode='lines',
+                    name='Actual Value')))
+fig.update_layout(
+    xaxis=dict(
+        showline=True,
+        showgrid=True,
+        showticklabels=False,
+        linecolor='white',
+        linewidth=2
+    ),
+    yaxis=dict(
+        title_text='Close (USD)',
+        titlefont=dict(
+            family='Rockwell',
+            size=12,
+            color='white',
+        ),
+        showline=True,
+        showgrid=True,
+        showticklabels=True,
+        linecolor='white',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Rockwell',
+            size=12,
+            color='white',
+        ),
+    ),
+    showlegend=True,
+    template = 'plotly_dark'
+
+)
+
+
+
+annotations = []
+annotations.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
+                              xanchor='left', yanchor='bottom',
+                              text='Results (GRU)',
+                              font=dict(family='Rockwell',
+                                        size=26,
+                                        color='white'),
+                              showarrow=False))
+fig.update_layout(annotations=annotations)
+
+fig.show()
+
+
+
+print("done")
